d6f9264b74dd01141c5cb0c1f595426bbaa38660
joaovictorpietgon <52017901+JoaoV-PGoncalves@users.noreply.github.com>
2022-01-11 15:23:59 -0300

    Add files via upload

---
+++ wp-login.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+	<html lang="pt-PT">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Iniciar sessão &lsaquo; André Gonzaga Expert &#8212; WordPress</title>
+	<meta name='robots' content='noindex, follow' />
+<link rel='dns-prefetch' href='//i0.wp.com' />
+<link rel='dns-prefetch' href='//i1.wp.com' />
+<link rel='dns-prefetch' href='//i2.wp.com' />
+<link rel='dns-prefetch' href='//c0.wp.com' />
+<link rel='stylesheet' id='dashicons-css'  href='https://c0.wp.com/c/5.8.3/wp-includes/css/dashicons.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://c0.wp.com/c/5.8.3/wp-includes/css/buttons.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://c0.wp.com/c/5.8.3/wp-admin/css/forms.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://c0.wp.com/c/5.8.3/wp-admin/css/l10n.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://c0.wp.com/c/5.8.3/wp-admin/css/login.min.css' type='text/css' media='all' />
+<link rel='stylesheet' id='anr-login-style-css'  href='https://andregonzagaexpert.com.br/wp-content/plugins/advanced-nocaptcha-recaptcha/assets/css/style.css?ver=5.8.3' type='text/css' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-pt-pt">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://pt.wordpress.org/">Criado com WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://andregonzagaexpert.com.br/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Nome de utilizador ou endereço de email</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Senha</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Mostrar senha">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+			<div class="anr_captcha_field"><div id="anr_captcha_field_1" class="anr_captcha_field_div"></div></div>			<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Lembrar-me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Iniciar sessão" />
+									<input type="hidden" name="redirect_to" value="https://andregonzagaexpert.com.br/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="https://andregonzagaexpert.com.br/wp-login.php?action=lostpassword">Esqueceu-se da senha?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="https://andregonzagaexpert.com.br/">&larr; Ir para André Gonzaga Expert</a>		</p>
+			</div>
+	<link rel='stylesheet' id='jetpack_css-css'  href='https://andregonzagaexpert.com.br/wp-content/plugins/jetpack/css/jetpack.css?ver=10.4' type='text/css' media='all' />
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/jquery/jquery.min.js' id='jquery-core-js'></script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/jquery/jquery-migrate.min.js' id='jquery-migrate-js'></script>
+<script type='text/javascript' id='zxcvbn-async-js-extra'>
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/andregonzagaexpert.com.br\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/zxcvbn-async.min.js' id='zxcvbn-async-js'></script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/dist/vendor/regenerator-runtime.min.js' id='regenerator-runtime-js'></script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/dist/vendor/wp-polyfill.min.js' id='wp-polyfill-js'></script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/dist/hooks.min.js' id='wp-hooks-js'></script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/dist/i18n.min.js' id='wp-i18n-js'></script>
+<script type='text/javascript' id='wp-i18n-js-after'>
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script type='text/javascript' id='password-strength-meter-js-extra'>
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Seguran\u00e7a da senha desconhecida","short":"Fraqu\u00edssima","bad":"Fraca","good":"M\u00e9dia","strong":"Forte","mismatch":"N\u00e3o s\u00e3o iguais"};
+/* ]]> */
+</script>
+<script type='text/javascript' id='password-strength-meter-js-translations'>
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", { "locale_data": { "messages": { "": {} } } } );
+</script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-admin/js/password-strength-meter.min.js' id='password-strength-meter-js'></script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/underscore.min.js' id='underscore-js'></script>
+<script type='text/javascript' id='wp-util-js-extra'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-includes/js/wp-util.min.js' id='wp-util-js'></script>
+<script type='text/javascript' id='user-profile-js-extra'>
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"0792aedf9b"};
+/* ]]> */
+</script>
+<script type='text/javascript' id='user-profile-js-translations'>
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", { "locale_data": { "messages": { "": {} } } } );
+</script>
+<script type='text/javascript' src='https://c0.wp.com/c/5.8.3/wp-admin/js/user-profile.min.js' id='user-profile-js'></script>
+			<script type="text/javascript">
+				var anr_onloadCallback = function() {
+					for ( var i = 0; i < document.forms.length; i++ ) {
+						var form = document.forms[i];
+						var captcha_div = form.querySelector( '.anr_captcha_field_div' );
+
+						if ( null === captcha_div )
+							continue;
+						captcha_div.innerHTML = '';
+						( function( form ) {
+							var anr_captcha = grecaptcha.render( captcha_div,{
+								'sitekey' : '6LcxZMobAAAAAKtm74KmmA2r_4ohmVJAaLY8k_M6',
+								'size'  : 'normal',
+								'theme' : 'light'
+							});
+							if ( typeof jQuery !== 'undefined' ) {
+								jQuery( document.body ).on( 'checkout_error', function(){
+									grecaptcha.reset(anr_captcha);
+								});
+							}
+							if ( typeof wpcf7 !== 'undefined' ) {
+								document.addEventListener( 'wpcf7submit', function() {
+									grecaptcha.reset(anr_captcha);
+								}, false );
+							}
+						})(form);
+					}
+				};
+			</script>
+						<script src="https://www.google.com/recaptcha/api.js?onload=anr_onloadCallback&#038;render=explicit"
+				async defer>
+			</script>
+				<div class="clear"></div>
+	</body>
+	</html>
+	<!-- wp-login.php -->
